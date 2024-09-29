535634d328d998d0118fa9bd77af0b27f0029c32
Shar <81372629+shar27@users.noreply.github.com>
2024-09-29 17:35:45 +0200

    remove locations

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -5,7 +5,6 @@ import Header from "../components/Sections/Header";
 import Services from "../components/Sections/Services";
 import Projects from "../components/Sections/Projects";
 import Reviews from "../components/Sections/Reviews";
-import Locations from "../components/Sections/Locations";
 import Blog from "../components/Sections/Blog";
 import Contact from "../components/Sections/Contact";
 import Footer from "../components/Sections/Footer"
@@ -18,7 +17,7 @@ export default function Landing() {
       <Header />
       <Reviews/>
       <Services />
-      <Locations/>
+      
       {/* <Projects /> */}
      
       <Contact />
